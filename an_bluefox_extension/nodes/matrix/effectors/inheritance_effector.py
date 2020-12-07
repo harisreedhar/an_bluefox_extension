99c81bee3a4c9babbdcbd28ce97adbc1ff28f1b7
Harisreedhar <harisreedhar@users.noreply.github.com>
2020-12-07 21:45:30 +0530

    added data mode selector
    this option can be found in advanced node setting

--- an_bluefox_extension/nodes/matrix/effectors/inheritance_effector.py
+++ an_bluefox_extension/nodes/matrix/effectors/inheritance_effector.py
@@ -3,13 +3,25 @@ from bpy.props import *
 from mathutils import Matrix
 from . effector_base import EffectorBase
 from animation_nodes . events import propertyChanged
-from animation_nodes . base_types import AnimationNode
 from animation_nodes . algorithms.rotations import directionsToMatrices
+from animation_nodes . base_types import AnimationNode, VectorizedSocket
 from animation_nodes . nodes . number . c_utils import range_DoubleList_StartStep
 from animation_nodes . nodes . spline . spline_evaluation_base import SplineEvaluationBase
-from . c_utils import inheritMatrixOverSpline, matrixTranslationLerp, matrixRotationLerp, matrixScaleLerp
-from animation_nodes . nodes . matrix . c_utils import setLocations, rotationsFromVirtualEulers, scalesFromVirtualVectors, multiplyMatrixLists
-from animation_nodes . data_structures import FloatList, VirtualMatrix4x4List, VirtualEulerList, VirtualVector3DList, Vector3DList, EulerList, Matrix4x4List
+
+from . c_utils import (
+    inheritMatrixOverSpline, matrixTranslationLerp,
+    matrixRotationLerp, matrixScaleLerp, inhertMatrixLinear,
+    vectorListLerp, inheritPointsOverSpline
+)
+from animation_nodes . nodes . matrix . c_utils import (
+    setLocations, rotationsFromVirtualEulers,
+    scalesFromVirtualVectors, multiplyMatrixLists
+)
+from animation_nodes . data_structures import (
+    FloatList, VirtualMatrix4x4List,
+    VirtualEulerList, VirtualVector3DList,
+    Vector3DList, EulerList, Matrix4x4List
+)
 
 trackAxisItems = [(axis, axis, "") for axis in ("X", "Y", "Z", "-X", "-Y", "-Z")]
 guideAxisItems  = [(axis, axis, "") for axis in ("X", "Y", "Z")]
@@ -17,6 +29,12 @@ guideAxisItems  = [(axis, axis, "") for axis in ("X", "Y", "Z")]
 selectModeItems = [
     ("LINEAR", "Linear", "Linear interpolation", "", 0),
     ("SPLINE", "Spline", "Along Curve", "", 1)
+]
+
+dataModeItems = [
+    ("MATRIX_COMPONENTS", "Matrices & Components", "", "", 0),
+    ("MATRIX_MATRIX", "Matrices & Matrices", "", "", 1),
+    ("VECTOR_VECTOR", "Vectors & Vectors", "", "", 2)
 ]
 
 class BF_InheritanceEffectorNode(bpy.types.Node, AnimationNode, EffectorBase, SplineEvaluationBase):
@@ -35,51 +53,107 @@ class BF_InheritanceEffectorNode(bpy.types.Node, AnimationNode, EffectorBase, Sp
     selectMode: EnumProperty(name = "Mode", default = "LINEAR",
         items = selectModeItems, update = AnimationNode.refresh)
 
-    def create(self):
-        self.newInput("Matrix List", "Matrices", "matrices", dataIsModified = True)
-        if self.selectMode == "SPLINE":
-            self.newInput("Spline", "Spline", "spline", defaultDrawType = "PROPERTY_ONLY")
-            self.newInput("Integer", "Samples", "samples", minValue = 1, value = 30)
-        self.createBasicInputs()
+    dataMode: EnumProperty(name = "Data Mode", default = "MATRIX_COMPONENTS",
+        items = dataModeItems, update = AnimationNode.refresh)
 
-        self.newOutput("Matrix List", "Matrices", "matrices")
-        self.newOutput("Float List", "Values", "effectorValues", hide = True)
-        self.updateSocketVisibility()
+    useMatrixList: VectorizedSocket.newProperty()
+    useVectorList: VectorizedSocket.newProperty()
+
+    def create(self):
+        if self.dataMode in ['MATRIX_COMPONENTS', 'MATRIX_MATRIX']:
+            if self.dataMode == 'MATRIX_MATRIX':
+                self.newInput("Matrix List", "Matrices 1", "matrices", dataIsModified = True)
+                self.newInput(VectorizedSocket("Matrix", "useMatrixList",
+                    ("Matrix 2", "matrices2"),
+                    ("Matrices 2", "matrices2")))
+                self.newInput("Falloff", "Falloff", "falloff")
+            else:
+                self.newInput("Matrix List", "Matrices", "matrices", dataIsModified = True)
+
+            if self.selectMode == "SPLINE":
+                self.newInput("Spline", "Spline", "spline", defaultDrawType = "PROPERTY_ONLY")
+                self.newInput("Integer", "Samples", "samples", minValue = 1, value = 30)
+            if self.dataMode == 'MATRIX_COMPONENTS':
+                self.createBasicInputs()
+            else:
+                self.createMinMaxInterpolationInputs()
+
+            self.newOutput("Matrix List", "Matrices", "matrices")
+            self.newOutput("Float List", "Values", "effectorValues", hide = True)
+            if self.dataMode == 'MATRIX_COMPONENTS':
+                self.updateSocketVisibility()
+
+        if self.dataMode == 'VECTOR_VECTOR':
+            self.newInput("Vector List", "Vectors 1", "vectors1", dataIsModified = True)
+            self.newInput(VectorizedSocket("Vector", "useVectorList",
+                    ("Vector 2", "vectors2"),
+                    ("Vectors 2", "vectors2")))
+            self.newInput("Falloff", "Falloff", "falloff")
+            if self.selectMode == "SPLINE":
+                self.newInput("Spline", "Spline", "spline", defaultDrawType = "PROPERTY_ONLY")
+                self.newInput("Integer", "Samples", "samples", minValue = 1, value = 30)
+            self.createMinMaxInterpolationInputs()
+
+            self.newOutput("Vector List", "Vectors", "vectors")
+            self.newOutput("Float List", "Values", "effectorValues", hide = True)
 
     def draw(self, layout):
         layout.prop(self, "selectMode", text = "")
-        if self.selectMode == "SPLINE":
-            row = layout.row(align = True)
-            label = "Align on spline"
-            if self.align:
-                label = ""
-            row.prop(self, "align", text = label)
-            if self.align:
-                row.prop(self, "trackAxis", index = 0, expand = True)
-                row2 = row.row(align = True)
-                row2.active = False
-                row2.prop(self, "guideAxis", index = 1, expand = True)
-                if self.trackAxis[-1:] == self.guideAxis[-1:]:
-                    layout.label(text = "Must be different", icon = "ERROR")
-        self.draw_MatrixTransformationProperties(layout)
+        if self.dataMode != 'VECTOR_VECTOR':
+            if self.selectMode == "SPLINE":
+                row = layout.row(align = True)
+                label = "Align on spline"
+                if self.align:
+                    label = ""
+                row.prop(self, "align", text = label)
+                if self.align:
+                    row.prop(self, "trackAxis", index = 0, expand = True)
+                    row2 = row.row(align = True)
+                    row2.active = False
+                    row2.prop(self, "guideAxis", index = 1, expand = True)
+                    if self.trackAxis[-1:] == self.guideAxis[-1:]:
+                        layout.label(text = "Must be different", icon = "ERROR")
+        if self.dataMode == 'MATRIX_COMPONENTS':
+            self.draw_MatrixTransformationProperties(layout)
 
     def drawAdvanced(self, layout):
+        layout.prop(self, "dataMode", text = "")
         if self.selectMode == "SPLINE":
             layout.prop(self, "resolution")
 
     def getExecutionCode(self, required):
-        if "matrices" in required or "effectorValues" in required:
-            yield "effectorValues = AN.data_structures.DoubleList()"
-            yield "newfalloff = self.remapFalloff(falloff, interpolation, outMin=minValue, outMax=maxValue)"
-            yield "influences = self.getInfluences(newfalloff, matrices)"
-            if self.selectMode == "SPLINE":
-                yield "matrices = self.executeSpline(matrices, spline, samples, translation, rotation, scale, influences)"
-            else:
-                yield "matrices = self.executeLinear(matrices, translation, rotation, scale, influences)"
-            if "effectorValues" in required:
-                    yield "effectorValues = AN.data_structures.DoubleList.fromValues(influences)"
+        if self.dataMode in ['MATRIX_COMPONENTS', 'MATRIX_MATRIX']:
+            if "matrices" in required or "effectorValues" in required:
+                yield "effectorValues = AN.data_structures.DoubleList()"
+                yield "newfalloff = self.remapFalloff(falloff, interpolation, outMin=minValue, outMax=maxValue)"
+                yield "influences = self.getInfluences(newfalloff, matrices)"
+                if self.selectMode == "SPLINE":
+                    if self.dataMode == 'MATRIX_COMPONENTS':
+                        yield "matrices = self.executeSpline_MAT_COMP(matrices, spline, samples, translation, rotation, scale, influences)"
+                    if self.dataMode == 'MATRIX_MATRIX':
+                        yield "matrices = self.executeSpline_MAT_MAT(matrices, matrices2, spline, samples, influences)"
+                else:
+                    if self.dataMode == 'MATRIX_COMPONENTS':
+                        yield "matrices = self.executeLinear_MAT_COMP(matrices, translation, rotation, scale, influences)"
+                    if self.dataMode == 'MATRIX_MATRIX':
+                        yield "matrices = self.executeLinear_MAT_MAT(matrices, matrices2, influences)"
+                if "effectorValues" in required:
+                        yield "effectorValues = AN.data_structures.DoubleList.fromValues(influences)"
 
-    def executeLinear(self, matrices, translation, rotation, scale, influences):
+        if self.dataMode == 'VECTOR_VECTOR':
+            if "vectors" in required or "effectorValues" in required:
+                yield "effectorValues = AN.data_structures.DoubleList()"
+                yield "newfalloff = self.remapFalloff(falloff, interpolation, outMin=minValue, outMax=maxValue)"
+                yield "evaluator = newfalloff.getEvaluator('LOCATION')"
+                yield "influences = evaluator.evaluateList(vectors1)"
+                if self.selectMode == "SPLINE":
+                    yield "vectors = self.executeSpline_VEC_VEC(vectors1, vectors2, spline, samples, influences)"
+                else:
+                    yield "vectors = self.executeLinear_VEC_VEC(vectors1, vectors2, influences)"
+                if "effectorValues" in required:
+                        yield "effectorValues = AN.data_structures.DoubleList.fromValues(influences)"
+
+    def executeLinear_MAT_COMP(self, matrices, translation, rotation, scale, influences):
         if not self.useTranslationList: translation = Vector3DList.fromValues([translation])
         if not self.useRotationList: rotation = EulerList.fromValues([rotation])
         if not self.useScaleList: scale = Vector3DList.fromValues([scale])
@@ -97,7 +171,18 @@ class BF_InheritanceEffectorNode(bpy.types.Node, AnimationNode, EffectorBase, Sp
             matrices = multiplyMatrixLists(matrices, scaleMatrices)
         return matrices
 
-    def executeSpline(self, matrices, spline, samples, translation, rotation, scale, influences):
+    def executeLinear_MAT_MAT(self, matrices, matrices2, influences):
+        if not self.useMatrixList: matrices2 = Matrix4x4List.fromValues([matrices2])
+        try:
+            return inhertMatrixLinear(matrices, matrices2, influences)
+        except:
+            return matrices
+
+    def executeLinear_VEC_VEC(self, vectors1, vectors2, influences):
+        if not self.useVectorList: vectors2 = Vector3DList.fromValues([vectors2])
+        return vectorListLerp(vectors1, vectors2, influences)
+
+    def executeSpline_MAT_COMP(self, matrices, spline, samples, translation, rotation, scale, influences):
         try:
             amount = len(matrices)
             if amount:
@@ -124,6 +209,32 @@ class BF_InheritanceEffectorNode(bpy.types.Node, AnimationNode, EffectorBase, Sp
                 return result
         except:
             return matrices
+
+    def executeSpline_MAT_MAT(self, matrices, matrices2, spline, samples, influences):
+        try:
+            amount = len(matrices)
+            if amount:
+                if not self.useMatrixList: matrices2 = Matrix4x4List.fromValues([matrices2])
+                matrices2 = VirtualMatrix4x4List.create(matrices2, Matrix.Identity(4)).materialize(amount)
+                samples = max(1, samples)
+                pathPoints, splineRotations = self.evalSpline(spline, samples)
+                result = inheritMatrixOverSpline(matrices, matrices2, pathPoints, splineRotations, influences, self.align)
+                return result
+        except:
+            return matrices
+
+    def executeSpline_VEC_VEC(self, vectors1, vectors2, spline, samples, influences):
+        try:
+            amount = len(vectors1)
+            if amount:
+                if not self.useVectorList: vectors2 = Vector3DList.fromValues([vectors2])
+                vectors2 = VirtualVector3DList.create(vectors2, (0,0,0)).materialize(amount)
+                samples = max(1, samples)
+                pathPoints, splineRotations = self.evalSpline(spline, samples)
+                result = inheritPointsOverSpline(vectors1, vectors2, pathPoints, influences)
+                return result
+        except:
+            return vectors1
 
     def evalSpline(self, spline, samples):
         spline.ensureUniformConverter(self.resolution)
