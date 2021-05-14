d60d53e45549f8d1ae19b65370547010e9b67cbd
Harisreedhar <46858047+harisreedhar@users.noreply.github.com>
2021-05-14 22:38:09 +0530

    Update Chaotic_attractors.pyx

--- an_bluefox_extension/nodes/generator/Chaotic_attractors.pyx
+++ an_bluefox_extension/nodes/generator/Chaotic_attractors.pyx
@@ -16,6 +16,7 @@ chaoticModelTypeItems = [
     ("AIZAWA", "Aizawa", "", "", 6),
     ("WANG", "Wang", "", "", 7),
     ("NOSE_HOOVER", "Nose Hoover", "", "", 8),
+    ("CHEN", "Chen", "", "", 9),
 ]
 
 class BF_ChaoticAttractorsNode(bpy.types.Node, AnimationNode):
@@ -33,159 +34,154 @@ class BF_ChaoticAttractorsNode(bpy.types.Node, AnimationNode):
         self.newInput("Float", "Time Step", "dt", value = 0.01)
         self.newInput("Vector", "Intial Vector", "initialVector", value = Vector((0.1, 0, -0.1)))
         if self.chaoticModel == 'LORENZ':
-            self.newInput("Float", "Param A", "paramA1", value = 10)
-            self.newInput("Float", "Param B", "paramB1", value = 28)
-            self.newInput("Float", "Param C", "paramC1", value = 2.6667)
-            self.newInput("Float", "Param D", "paramD1", hide = True)
+            self.createFloatInputs(values=[10, 28, 2.6667])
         if self.chaoticModel == 'ROSSLER':
-            self.newInput("Float", "Param A", "paramA2", value = 0.2)
-            self.newInput("Float", "Param B", "paramB2", value = 0.2)
-            self.newInput("Float", "Param C", "paramC2", value = 5.7)
-            self.newInput("Float", "Param D", "paramD2", hide = True)
+            self.createFloatInputs(values=[0.2, 0.2, 5.7])
         if self.chaoticModel == 'DEQUAN_LI':
-            self.newInput("Float", "Param A", "paramA3", value = 40)
-            self.newInput("Float", "Param B", "paramB3", value = 1.833)
-            self.newInput("Float", "Param C", "paramC3", value = 0.16)
-            self.newInput("Float", "Param D", "paramD3", value = 0.65)
+            self.createFloatInputs(values=[40, 1.833, 0.16, 0.65, 55, 20])
         if self.chaoticModel == 'RIKITAKE':
-            self.newInput("Float", "Param A", "paramA4", value = 5)
-            self.newInput("Float", "Param B", "paramB4", value = 2)
-            self.newInput("Float", "Param C", "paramC4", hide = True)
-            self.newInput("Float", "Param D", "paramD4", hide = True)
+            self.createFloatInputs(values=[5, 2])
         if self.chaoticModel == 'CHUA':
-            self.newInput("Float", "Param A", "paramA5", value = 15.6)
-            self.newInput("Float", "Param B", "paramB5", value = 28)
-            self.newInput("Float", "Param C", "paramC5", value = -1.143)
-            self.newInput("Float", "Param D", "paramD5", value = -0.714)
+            self.createFloatInputs(values=[15.6, 28, -1.143, -0.714])
         if self.chaoticModel == 'HADLEY':
-            self.newInput("Float", "Param A", "paramA6", value = 0.2)
-            self.newInput("Float", "Param B", "paramB6", value = 4)
-            self.newInput("Float", "Param C", "paramC6", value = 8)
-            self.newInput("Float", "Param D", "paramD6", value = 1)
+            self.createFloatInputs(values=[0.2, 4, 8, 1])
         if self.chaoticModel == 'AIZAWA':
-            self.newInput("Float", "Param A", "paramA7", value = 0.95)
-            self.newInput("Float", "Param B", "paramB7", value = 0.7)
-            self.newInput("Float", "Param C", "paramC7", value = 0.6)
-            self.newInput("Float", "Param D", "paramD7", value = 3.5)
+            self.createFloatInputs(values=[0.95, 0.7, 0.6, 3.5])
+        if self.chaoticModel == 'CHEN':
+            self.createFloatInputs(values=[35, 2.6667, 28])
         if self.chaoticModel in ('WANG', 'NOSE_HOOVER'):
-            self.newInput("Float", "Param A", "paramA8", value = 1)
-            self.newInput("Float", "Param B", "paramB8", value = 1)
-            self.newInput("Float", "Param C", "paramC8", value = 1)
-            self.newInput("Float", "Param D", "paramD8", hide = True)
-
+            self.createFloatInputs(values=[1, 1, 1])
         self.newInput("Float", "Scale", "scale", value = 1)
 
         self.newOutput("Vector List", "Vectors", "vectors")
 
+    def createFloatInputs(self, values=[], number=6):
+        name = self.chaoticModel.lower()
+        sockets = []
+        for i in range(number):
+            charValue = chr(65 + i)
+            inputName = f"{name}_param_{charValue}"
+            socket = self.newInput("Float", f"Param {charValue}", inputName, value = 1, hide = True)
+            sockets.append(socket)
+        for i, value in enumerate(values):
+            sockets[i].value = value
+            sockets[i].hide = False
+
     def draw(self, layout):
         layout.prop(self, "chaoticModel", text = "")
 
-    def execute(self, iterations, dt, initialVector, paramA, paramB, paramC, paramD, scale):
-        vectors = attractor(iterations, dt, initialVector,
-                            paramA, paramB, paramC, paramD,
-                            scale, self.chaoticModel)
+    def execute(self, *args):
+        chaoticModel = self.chaoticModel
+        attractor = Lorenz(args)
+        if chaoticModel == "ROSSLER":
+            attractor = Rossler(args)
+        elif chaoticModel == "DEQUAN_LI":
+            attractor = DequanLi(args)
+        elif chaoticModel == "RIKITAKE":
+            attractor = Rikitake(args)
+        elif chaoticModel == "CHUA":
+            attractor = Chua(args)
+        elif chaoticModel == "HADLEY":
+            attractor = Hadley(args)
+        elif chaoticModel == "AIZAWA":
+            attractor = Aizawa(args)
+        elif chaoticModel == "WANG":
+            attractor = Wang(args)
+        elif chaoticModel == "NOSE_HOOVER":
+            attractor = NoseHoover(args)
+        elif chaoticModel == "CHEN":
+            attractor = Chen(args)
+        vectors = attractor.generate()
         return vectors
 
-cdef Vector3DList attractor(Py_ssize_t iterations,
-                            float dt,
-                            initialVector,
-                            float paramA,
-                            float paramB,
-                            float paramC,
-                            float paramD,
-                            float scale,
-                            str chaoticModel):
+cdef class AttractorBase:
+    cdef Py_ssize_t iterations
+    cdef float dt, x, y, z, paramA, paramB, paramC, paramD, paramE, paramF, scale
 
-    cdef float x, y, z, var
-    cdef Vector3DList vectors = Vector3DList(length = iterations)
-    cdef Py_ssize_t i
+    def __cinit__(self, args):
+        self.iterations = args[0]
+        self.dt = args[1]
+        self.x = args[2].x
+        self.y = args[2].y
+        self.z = args[2].z
+        self.paramA = args[3]
+        self.paramB = args[4]
+        self.paramC = args[5]
+        self.paramD = args[6]
+        self.paramE = args[7]
+        self.paramF = args[8]
+        self.scale = args[9]
 
-    x = initialVector.x
-    y = initialVector.y
-    z = initialVector.z
+    cdef void attractor(self):
+        pass
 
-    iterations = max(iterations, 1)
+    cdef Vector3DList generate(self):
+        cdef Vector3DList vectors = Vector3DList(length = self.iterations)
+        for i in range(self.iterations):
+            self.attractor()
+            vectors.data[i].x = self.x * self.scale
+            vectors.data[i].y = self.y * self.scale
+            vectors.data[i].z = self.z * self.scale
+        return vectors
 
-    if chaoticModel == "LORENZ":
-        for i in range(iterations):
-            x += (paramA * (y - x)) * dt
-            y += (paramB * x - y - x * z) * dt
-            z += (x * y - paramC * z) * dt
-            vectors.data[i].x = x * scale
-            vectors.data[i].y = y * scale
-            vectors.data[i].z = z * scale
+cdef class Lorenz(AttractorBase):
+    cdef void attractor(self):
+        self.x += (self.paramA * (self.y - self.x)) * self.dt
+        self.y += (self.paramB * self.x - self.y - self.x * self.z) * self.dt
+        self.z += (self.x * self.y - self.paramC * self.z) * self.dt
 
-    elif chaoticModel == "ROSSLER":
-        for i in range(iterations):
-            x += -(y + z) * dt
-            y += (x + paramA * y) * dt
-            z += (paramB + z * (x - paramC)) * dt
-            vectors.data[i].x = x * scale
-            vectors.data[i].y = y * scale
-            vectors.data[i].z = z * scale
+cdef class Rossler(AttractorBase):
+    cdef void attractor(self):
+        self.x += -(self.y + self.z) * self.dt
+        self.y += (self.x + self.paramA * self.y) * self.dt
+        self.z += (self.paramB + self.z * (self.x - self.paramC)) * self.dt
 
-    elif chaoticModel == "DEQUAN_LI":
-        for i in range(iterations):
-            x += (paramA * (y - x) + paramC * x * z) * dt * 0.1
-            y += (55 * x + 20 * y - x * z) * dt * 0.1
-            z += (paramB * z + x * y - paramD * x * x) * dt * 0.1
-            vectors.data[i].x = x * scale
-            vectors.data[i].y = y * scale
-            vectors.data[i].z = z * scale
+cdef class DequanLi(AttractorBase):
+    cdef void attractor(self):
+        self.x += (self.paramA * (self.y - self.x) + self.paramC * self.x * self.z) * self.dt * 0.1
+        self.y += (self.paramE * self.x + self.paramF * self.y - self.x * self.z) * self.dt * 0.1
+        self.z += (self.paramB * self.z + self.x * self.y - self.paramD * self.x * self.x) * self.dt * 0.1
 
-    elif chaoticModel == "RIKITAKE":
-        for i in range(iterations):
-            x += (-paramB * x + z * y) * dt
-            y += (-paramB * y + x * (z - paramA)) * dt
-            z += (1 - x * y) * dt
-            vectors.data[i].x = x * scale
-            vectors.data[i].y = y * scale
-            vectors.data[i].z = z * scale
+cdef class Rikitake(AttractorBase):
+    cdef void attractor(self):
+        self.x += (-self.paramB * self.x + self.z * self.y) * self.dt
+        self.y += (-self.paramB * self.y + self.x * (self.z - self.paramA)) * self.dt
+        self.z += (1 - self.x * self.y) * self.dt
 
-    elif chaoticModel == "CHUA":
-        for i in range(iterations):
-            var = paramD * x + 0.5 * (paramC - paramD) * (fabs(x + 1) - fabs(x - 1))
-            x += (paramA * (y - x - var)) * dt
-            y += (x - y + z) * dt
-            z += (-paramB * y) * dt
-            vectors.data[i].x = x * scale
-            vectors.data[i].y = y * scale
-            vectors.data[i].z = z * scale
+cdef class Chua(AttractorBase):
+    cdef void attractor(self):
+        cdef float var = self.paramD * self.x + 0.5 * (self.paramC - self.paramD) * (fabs(self.x + 1) - fabs(self.x - 1))
+        self.x += (self.paramA * (self.y - self.x - var)) * self.dt
+        self.y += (self.x - self.y + self.z) * self.dt
+        self.z += (-self.paramB * self.y) * self.dt
 
-    elif chaoticModel == "HADLEY":
-        for i in range(iterations):
-            x += (-y**2 - z**2 - paramA * x + paramA * paramC) * dt
-            y += (x * y - paramB * x * z - y + paramD) * dt
-            z += (paramB * x * y + x * z - z) * dt
-            vectors.data[i].x = x * scale
-            vectors.data[i].y = y * scale
-            vectors.data[i].z = z * scale
+cdef class Hadley(AttractorBase):
+    cdef void attractor(self):
+        self.x += (-self.y**2 - self.z**2 - self.paramA * self.x + self.paramA * self.paramC) * self.dt
+        self.y += (self.x * self.y - self.paramB * self.x * self.z - self.y + self.paramD) * self.dt
+        self.z += (self.paramB * self.x * self.y + self.x * self.z - self.z) * self.dt
 
-    elif chaoticModel == "AIZAWA":
-        for i in range(iterations):
-            x += ((z - paramB) * x - paramD * y) * dt
-            y += (paramD * x + (z - paramB) * y) * dt
-            z += (paramC + paramA * z - (z**3 / 3) - (x*x + y*y) * (1 + 0.25 * z) + 0.1 * z * x**3) * dt
-            vectors.data[i].x = x * scale
-            vectors.data[i].y = y * scale
-            vectors.data[i].z = z * scale
+cdef class Aizawa(AttractorBase):
+    cdef void attractor(self):
+        self.x += ((self.z - self.paramB) * self.x - self.paramD * self.y) * self.dt
+        self.y += (self.paramD * self.x + (self.z - self.paramB) * self.y) * self.dt
+        self.z += (self.paramC + self.paramA * self.z - (self.z**3 / 3) - (self.x*self.x + self.y*self.y) *
+                  (1 + 0.25 * self.z) + 0.1 * self.z * self.x**3) * self.dt
 
-    elif chaoticModel == "WANG":
-        for i in range(iterations):
-            x += (x - y * z) * paramA * dt
-            y += (x - y + x * z) * paramB * dt
-            z += (-3 * z + x * y) * paramC * dt
-            vectors.data[i].x = x * scale
-            vectors.data[i].y = y * scale
-            vectors.data[i].z = z * scale
+cdef class Wang(AttractorBase):
+    cdef void attractor(self):
+        self.x += (self.x - self.y * self.z) * self.paramA * self.dt
+        self.y += (self.x - self.y + self.x * self.z) * self.paramB * self.dt
+        self.z += (-3 * self.z + self.x * self.y) * self.paramC * self.dt
 
-    elif chaoticModel == "NOSE_HOOVER":
-        for i in range(iterations):
-            x += y * paramA * dt
-            y += (y * z - x) * paramB * dt
-            z += (1 - y * y) * paramC * dt
-            vectors.data[i].x = x * scale
-            vectors.data[i].y = y * scale
-            vectors.data[i].z = z * scale
+cdef class NoseHoover(AttractorBase):
+    cdef void attractor(self):
+        self.x += self.y * self.paramA * self.dt
+        self.y += (self.y * self.z - self.x) * self.paramB * self.dt
+        self.z += (1 - self.y * self.y) * self.paramC * self.dt
 
-    return vectors
+cdef class Chen(AttractorBase):
+    cdef void attractor(self):
+        self.x += self.paramA * (self.y - self.x) * self.dt * 0.1
+        self.y += ((self.paramC - self.paramA) * self.x - self.x * self.z + self.paramC * self.y) * self.dt * 0.1
+        self.z += (self.x * self.y - self.paramB * self.z) * self.dt * 0.1
